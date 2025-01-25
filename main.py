0878632f06d99edf50df75b1e5057f7e3c85b50b
romofel <felipe@romofel.dev>
2025-01-25 09:20:17 -0600

    yikes

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 def main():
     BOOK_PATH = "./books/frankenstein.txt"
     with open(BOOK_PATH) as book:
-        database = {}
         book_contents = book.read()
-        for letter in book_contents:
-            lower_letter = letter.lower()
-            if lower_letter in database:
-                database[lower_letter] += 1
-            else:
-                database[lower_letter] = 1
-        print(database)
+        generate_report(book_contents)
 
-def generate_report(database):
+
+def get_word_count(text):
+    return len(text.split())
+
+def generate_database(text):
+    database = {}
+    for letter in text:
+        lower_letter = letter.lower()
+        if lower_letter in database:
+            database[lower_letter] += 1
+        else:
+            database[lower_letter] = 1
+    return database
+
+
+def generate_report(text):
     print("--- Begin report of books/frankenstein.txt ---")
-    for row in database:
-        print(row)
-
+    print(f"{get_word_count(text)} words found in the document")
+    database = generate_database(text)
+    for key, value in sorted(database.items(), key=lambda entry: entry[1], reverse=True):
+        if key.isalpha():
+            print(f"The '{key}' character was found {value}")
+    print("--- End report ---")
 if __name__ == "__main__":
     main()
 
